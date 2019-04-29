2a59f0f6cda53372a8de74895b8d9cad06b048d4
mizuki <midukihimmel@gmail.com>
2019-04-29 18:27:05 +0900

    ファイルがどういう動きをするのかコメントを追記 変数の役割にコメントを追加 コピー元ファイル名の指定が間違っていたので修正

--- createBackup.bat
+++ createBackup.bat
@@ -1,11 +1,17 @@
 @echo off
 cd /d %~dp0
+rem 指定したファイル名のファイルのバックアップファイルを生成する
+rem ./YYYY/MM/[バックファイル名]の形式で生成される
 
-set copyFileName="バックアップを行うファイル名"
-set bkFolderName="バックアップフォルダ名"
-set bkfileName="バックアップ後のファイル名"
+rem バックアップするファイル名
+set copyFileName="README.md"
+rem バックアップフォルダ名
+set bkFolderName="backup"
+rem 生成するバックアップファイル名（先頭にYYYYMMDD_が付与される）
+set bkfileName=".BAK"
 
 for /f "tokens=1-3 delims=/" %%a in ("%date%") do (
+
 	mkdir %bkFolderName%\%%a\%%b
-	copy YYYYMMDD.txt %bkFolderName%\%%a\%%b\%%a%%b%%c%bkfileName%.BAK
+	copy %copyFileName% %bkFolderName%\%%a\%%b\%%a%%b%%c"_"%bkfileName%
 )
