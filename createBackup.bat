b81e9e8441d19f5a3d229632a7515c45f36dcb2d
mizukiRe <49385979+mizukiRe@users.noreply.github.com>
2019-11-29 10:43:53 +0900

    コメントを修正

--- createBackup.bat
+++ createBackup.bat
@@ -1,11 +1,11 @@
 @echo off
 cd /d %~dp0
-rem 指定したファイル名のファイルのバックアップファイルを生成する
-rem ./YYYY/MM/[バックファイル名]の形式で生成される
+rem 指定したファイルのバックアップを生成する
+rem バックアップするファイルと同じディレクトリに置くか、バックアップするファイル名にフルパスを指定する
 
 rem バックアップするファイル名
 set copyFileName="README.md"
-rem バックアップフォルダ名
+rem バックアップフォルダ名（存在しなければ自動生成）
 set bkFolderName="backup"
 rem 生成するバックアップファイル名（先頭にYYYYMMDD_が付与される）
 set bkfileName=".BAK"
